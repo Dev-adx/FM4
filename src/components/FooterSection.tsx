ba2e9b8df0a1b2bd8aaae06a7aac47f8b11d72e8
faizal-05 <faizalkhan.0517@gmail.com>
2026-03-13 15:41:56 +0530

    Initial clean commit

--- src/components/FooterSection.tsx
+++ src/components/FooterSection.tsx
@@ -28,7 +28,7 @@ const FooterSection = () => (
     </div>
 
     {/* Bottom copyright bar */}
-    <div className="border-t border-gray-200 mb-20">
+    <div className="border-t border-gray-200">
       <div className="container max-w-5xl py-2 text-center text-sm text-muted-foreground">
         Copyright © 2025 Fitness Master. All rights reserved.&nbsp;
         <a href="/refund-policy" className="underline hover:text-primary">Refund Policy</a>
